b363381d4867059ec10d01638ecfb013d38594d5
Bull <parsaayani9@gmail.com>
2016-05-31 17:29:51 -0400

    cleaned up spaghetti networkvar code in anticipation for duplicator

--- lua/entities/gmod_safespace/init.lua
+++ lua/entities/gmod_safespace/init.lua
@@ -5,12 +5,14 @@ include('shared.lua')
 
 ENT:AddHook("PlayerInitialize", "interior", function(self)
 	net.WriteTable(self.dimensions)
+	net.WriteString(self.material)
+	net.WriteString(self.surfacetype)
 end)
 
 function ENT:Initialize()
 	self.dimensions=SafeSpace:GetExteriorDimensions(self:GetCreator())
 	self.Portal=self:GetPortalDimensions()
+	self.material = SafeSpace:GetTextureExterior(self:GetCreator()) or "models/props_wasteland/wood_fence01a"
+	self.surfacetype = SafeSpace:GetSurfaceProperty(self:GetCreator()) or "metal"
 	self.BaseClass.Initialize(self)
-	self:SetNWString("safespace_texture_exterior",SafeSpace:GetTextureExterior(self:GetCreator()) or "sprops/sprops_grid_12x12")
-	self:SetNWString("safespace_surface",SafeSpace:GetSurfaceProperty(self:GetCreator()) or "metal")
 end
